a9a7175ebb225a2c5d611e89bc700ea7e4662259
zhu849 <66557634+zhu849@users.noreply.github.com>
2021-03-29 19:54:34 +0800

    Addition connection method and modify some parameter.
    First version of connection method, and still not test with serial device server.
    Change port name, parity and baud rate from string type to const type.

--- main.c
+++ main.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
-/*Temperature controller RS232 connection infomation*/
-#define BTC9100_CMB_PORTNAME	"COM6"
-#define BTC9100_CMB_PARITY		"Even"
+/* Temperature controller RS232 connection infomation */
+#define BTC9100_CMB_PORTNAME	"/dev/ttyUSB0"
+#define BTC9100_CMB_PARITY		PARENB /* 0 = None, PARENB = Even, PARODD = Odd */
 #define BTC9100_CMB_STOPBITS	1
 #define BTC9100_CMB_DATABITS	8
-#define BTC9100_CMB_BAUDRATE	9600
+#define BTC9100_CMB_BAUDRATE	B9600 /* B + baudrate */
 #define BTC9100_SLAVE_ID		"2"
 
-/*Fan controller RS232 connection information*/
-#define EZD305F_CMB_PORTNAME	"COM6"
-#define EZD305F_CMB_PARITY		"None"
+/* Fan controller RS232 connection information */
+#define EZD305F_CMB_PORTNAME	"/dev/ttyUSB0"
+#define EZD305F_CMB_PARITY		0 /* 0 = None, PARENB = Even, PARODD = Odd */
 #define EZD305F_CMB_STOPBITS	1
 #define EZD305F_CMB_DATABITS	8
-#define EZD305F_CMB_BAUDRATE	9600
+#define EZD305F_CMB_BAUDRATE	B9600 /* B + baudrate */
 #define EZD305F_SLAVE_ID		"0A"
 
-/*Static Number*/
+/* Static Number */
 #define MAX_STR_LENS 50
 
-/*Command Define*/
+
+/* Command Define */
 #define TEMP_READ	0
 #define TEMP_SETUP	1
 #define FAN_SETUP	2
@@ -30,9 +36,11 @@
 #define BULB_ON		6
 #define BULB_OFF	7
 
-/*Global variable*/
+/* Global variable */
+int comport;//File descriptor
+struct termios newPortSetting, oldPortSetting;
 
-/*Function*/
+/* Function */
 static int cmdConvertToNum(char*);
 static void tempRead();
 static void tempSetup();
@@ -44,48 +52,8 @@ static void bulbOn();
 static void bulbOff();
 static int setConnectionInfo();
 
-int main() {
-	char args[2][MAX_STR_LENS] = { "TempSetup", "48.7" };//For test
-	//// Connection Myconnect = new Connection();
-	//// SerialPort comport = new SerialPort();
-	//// Modbus.Device.ModbusSerialMaster master = null;
-	////comport.Close();
 
-	//Set information of RS232 connection like port name, parity bits, stop bits, data bits, baudrate and slave ID.
-	if (!setConnectionInfo()) {
-		printf("Fail to set connection with chamber.");
-		return 0;
-	};
-	
-	switch(cmdConvertToNum(args[0])){
-		case TEMP_READ:
-			tempRead();
-			break;
-		case TEMP_SETUP:
-			tempSetup();
-			break;
-		case FAN_SETUP:
-			fanSetup();
-			break;
-		case FAN_ON:
-			fanOn();
-			break;
-		case FAN_OFF:
-			fanOff();
-			break;
-		case BULB_ON:
-			bulbOn();
-			break;
-		case BULB_OFF:
-			bulbOff();
-			break;
-		default:
-			printf("Error command or command convert failured.\n");
-	};
-	return 0;
-}
-
-//Convert command of string type to corresponding number  
+/* Convert command of string type to corresponding number */  
 int cmdConvertToNum(char *str) {
 	if (strcmp(str, "TempRead")) return TEMP_READ;
 	else if(strcmp(str, "TempSetup")) return TEMP_SETUP;
@@ -96,10 +64,63 @@ int cmdConvertToNum(char *str) {
 	else if (strcmp(str, "BulbOff")) return BULB_OFF;
 }
 
-//Set Information about serial port connection
+/* Set Information about serial port connection */
 int setConnectionInfo() {
+	int status;
 
-	return 0;//Fail to set connection
+	comport = open(BTC9100_CMB_PORTNAME, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (comport == -1) {
+		printf("Unable to open comport with %s\n", BTC9100_CMB_PORTNAME);
+		return 0;/* Fail to set connection */
+	}
+	
+	/* Get parameters associated with the object referred by fd and stores them in the termios structure.*/
+	if (tcgetattr(comport, &oldPortSetting) == -1) {
+		close(comport);
+		printf("Unable to read port setting with %s\n", BTC9100_CMB_PORTNAME);
+		return 0;
+	}
+	memset(&newPortSetting, 0, sizeof(newPortSetting)); /* Initialize new port setting struct */
+	/* Set new port setting struct */
+	newPortSetting.c_cflag = BTC9100_CMB_DATABITS | BTC9100_CMB_PARITY | BTC9100_CMB_STOPBITS | CLOCAL | CREAD;
+	newPortSetting.c_oflag = 0;
+	newPortSetting.c_lflag = 0;
+	newPortSetting.c_cc[VMIN] = 0;
+	newPortSetting.c_cc[VTIME] = 0;
+
+	/* Set input baud rate in termios structure */
+	if (cfsetispeed(&newPortSetting, BTC9100_CMB_BAUDRATE) == -1) {
+		close(comport);
+		printf("Unable to set input baud rate with %s\n", BTC9100_CMB_PORTNAME);
+		return 0;
+	}
+	/* Set output baud rate in termios structure */
+	if (cfsetospeed(&newPortSetting, BTC9100_CMB_BAUDRATE) == -1) {
+		close(comport);
+		printf("Unable to set output baud rate with %s\n", BTC9100_CMB_PORTNAME);
+		return 0;
+	}
+	/* Set parameters into comport's termios structure.*/
+	if (tcsetattr(comport, TCSANOW, &newPortSetting) == -1) {
+		tcsetattr(comport, TCSANOW, &oldPortSetting);/* Restore to old port setting */
+		close(comport);
+		printf("Unable to set port setting with %s.\n", BTC9100_CMB_PORTNAME);
+		return 0;
+	}
+	/* Get the status of modem bits */
+	if (ioctl(comport, TIOCMGET, &status) == -1) {
+		tcsetattr(comport, TCSANOW, &oldPortSetting);/* Restore to old port setting */
+		printf("Unable to get port setting with %s.\n", BTC9100_CMB_PORTNAME);
+		return 0;
+	}
+	status |= TIOCM_DTR | TIOCM_RTS;/* Configure status data terminal ready and request to send */
+	/* Set the status of modem bits */
+	if (ioctl(comport, TIOCMSET, &status) == -1) {
+		tcsetattr(comport, TCSANOW, &oldPortSetting);/* Restore to old port setting */
+		printf("Unable to set port setting with %s.\n", BTC9100_CMB_PORTNAME);
+		return 0;
+	}
+	return 1;/* Success to set connection */
 }
 
 //Get temperature from BTC-9100
@@ -140,4 +161,45 @@ void bulbOn() {
 //Turn off bulb
 void bulbOff() {
 
+}
+
+int main() {
+	char args[2][MAX_STR_LENS] = { "TempSetup", "48.7" };//For test
+	//// Connection Myconnect = new Connection();
+	//// SerialPort comport = new SerialPort();
+	//// Modbus.Device.ModbusSerialMaster master = null;
+	////comport.Close();
+
+	/* Set information of RS232 connection like port name, parity bits, stop bits, data bits, baudrate and slave ID. */
+	if (!setConnectionInfo()) {
+		printf("Fail to set connection with chamber.");
+		return 0;
+	};
+
+	switch (cmdConvertToNum(args[0])) {
+	case TEMP_READ:
+		tempRead();
+		break;
+	case TEMP_SETUP:
+		tempSetup();
+		break;
+	case FAN_SETUP:
+		fanSetup();
+		break;
+	case FAN_ON:
+		fanOn();
+		break;
+	case FAN_OFF:
+		fanOff();
+		break;
+	case BULB_ON:
+		bulbOn();
+		break;
+	case BULB_OFF:
+		bulbOff();
+		break;
+	default:
+		printf("Error command or command convert failured.\n");
+	};
+	return 0;
 }
